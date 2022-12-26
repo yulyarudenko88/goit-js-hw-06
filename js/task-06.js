faab5c8179f9730f14315460231175a27f08fe85
yulyarudenko88 <111283521+yulyarudenko88@users.noreply.github.com>
2022-12-26 12:28:46 +0200

    change task 6

--- js/task-06.js
+++ js/task-06.js
@@ -1,11 +1,18 @@
 const inputRef = document.getElementById("validation-input");
 
 function validation(event) {
-  if (Number(inputRef.dataset.length) > event.currentTarget.value.length) {
-    inputRef.classList.add("invalid");
-  } else {
+  if (Number(inputRef.dataset.length) === event.currentTarget.value.length) {
     inputRef.classList.add("valid");
-    inputRef.classList.remove("invalid");
+
+    if (inputRef.classList.contains("invalid")) {
+      inputRef.classList.remove("invalid");
+    }
+  } else {
+    inputRef.classList.add("invalid");
+
+    if (inputRef.classList.contains("valid")) {
+      inputRef.classList.remove("valid");
+    }
   }
 }
 
